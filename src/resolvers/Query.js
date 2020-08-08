9ea9313a05c6b006167ccd4582b84df1d3d18b6c
shjang7 <suhy.jang.shj@gmail.com>
2020-08-08 18:07:41 +0900

    Feat - Current User

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,7 +1,34 @@
+import { query } from 'express';
+import getUserId from '../utils/getUserId';
+
 const Query = {
   users(parent, args, { prisma }, info) {
-    const opArgs = {};
+    const opArgs = {
+      first: args.first,
+      skip: args.skip,
+      after: args.after,
+      orderBy: args.orderBy,
+    };
+
+    if (query) {
+      opArgs.where = {
+        name_contains: query,
+      };
+    }
+
     return prisma.query.users(opArgs, info);
+  },
+  me(parent, args, { prisma, request }, info) {
+    const userId = getUserId(request);
+
+    return prisma.query.user(
+      {
+        where: {
+          id: userId,
+        },
+      },
+      info,
+    );
   },
 };
 

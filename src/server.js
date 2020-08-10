e34cf48e0ee3715c1709cc7b69203d07bf5603a7
shjang7 <suhy.jang.shj@gmail.com>
2020-08-10 18:34:13 +0900

    Feat - Set security adding cors, express-rate-limit, helmet, hpp, xss-clean

--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
 import { GraphQLServer } from 'graphql-yoga';
 import prisma from './prisma';
 import { resolvers, fragmentReplacements } from './resolvers';
+import helmet from 'helmet';
+import xss from 'xss-clean';
+import rateLimit from 'express-rate-limit';
+import hpp from 'hpp';
+import cors from 'cors';
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
@@ -13,5 +18,25 @@ const server = new GraphQLServer({
   },
   fragmentReplacements,
 });
+
+// Set security headers
+server.express.use(helmet());
+
+// Prevent XSS attacks
+server.express.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 mins
+  max: 100, // limit each IP to 100 requests per windowMs
+  message: 'Too many requests, please try again after 15 minutes later.',
+});
+server.express.use(limiter);
+
+// Prevent http param pollution
+server.express.use(hpp());
+
+// Enable CORS
+server.express.use(cors());
 
 export { server as default };
